e2db31558f456a3cb9aa91100ce807093e8ab705
Benjamin Cheng <bencheng200@gmail.com>
2020-12-14 14:14:46 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,6 @@ class Dog
   attr_accessor :name, :breed, :id
 
   def initialize(attributes)
-    #id: nil, name:, breed:
     attributes.each {|key, value| self.send(("#{key}="), value)}
     self.id ||= nil
   end
